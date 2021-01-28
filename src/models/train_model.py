b29c0212cd6166bbc363e71cfb42cfcdda119722
Julio Oliveira <jcalvesoliveira@hotmail.com>
2021-01-28 15:53:11 -0300

    mlflow tracking, experiments and runs

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -39,7 +39,7 @@ def main():
 
     # enable auto logging
     mlflow.set_tracking_uri("http://localhost:5000") ## incluir para modelos registrados apenas
-    mlflow.set_experiment('curso-mlflow')
+    mlflow.set_experiment('house-prices-script')
     mlflow.xgboost.autolog()
 
     dtrain = xgboost.DMatrix(X_train, label=y_train)
