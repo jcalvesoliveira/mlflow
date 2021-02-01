294423d2172175d3e0093050c8ce83d073589e22
Julio Oliveira <jcalvesoliveira@hotmail.com>
2021-02-01 09:14:48 -0300

    adding mlproject

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,4 +1,5 @@
 import mlflow
+#alterar para modelo em producao ao final
 logged_model = 'file:///mnt/d/Google%20Drive/Jobs/Aulas/Alura/curso-mlflow/mlruns/0/51b8a608b9354411877fa4ad850ece8c/artifacts/model'
 
 # Load model as a PyFuncModel.
@@ -6,7 +7,7 @@ loaded_model = mlflow.pyfunc.load_model(logged_model)
 
 # Predict on a Pandas DataFrame.
 import pandas as pd
-data = pd.read_csv('data/processed/casas.csv')
+data = pd.read_csv('../../data/processed/casas.csv')
 predicted = loaded_model.predict(data)
 
 import numpy as np
